59fbb4d51d46fac006003d0e6d9cfcd75feeb303
yellowgh0st <augustin.be@protonmail.com>
2021-09-16 04:21:23 +0200

    Add nixos configs

---
+++ configuration.nix
@@ -0,0 +1,207 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  boot.supportedFilesystems = [ "btrfs" ];
+  hardware.enableAllFirmware = true;
+  hardware.cpu.amd.updateMicrocode = true;
+  nixpkgs.config.allowUnfree = true;
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
+  networking.hostName = "nixxx"; # Define your hostname.
+  networking.networkmanager.enable = true;  # Enables networking
+
+  # Set your time zone.
+  time.timeZone = "Europe/Prague";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.enp9s0.useDHCP = true;
+  networking.nameservers = [ "127.0.0.1" "::1" ];
+  networking.dhcpcd.extraConfig = "nohook resolv.conf";
+  networking.networkmanager.dns = "none";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+     font = "Lat2-Terminus16";
+     keyMap = "us";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.wayland = true;
+  services.xserver.desktopManager.xterm.enable = false;
+  services.xserver.desktopManager.gnome.enable = true;  
+  programs.xwayland.enable = false;
+  services.flatpak.enable = true;
+  # Configure keymap in X11
+  services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
+  hardware.opengl.extraPackages = with pkgs; [
+   rocm-runtime
+   rocm-opencl-icd
+   rocm-device-libs
+   rocm-opencl-runtime
+  ];
+
+  hardware.opengl.driSupport = true;
+  hardware.opengl.driSupport32Bit = true;
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  security.rtkit.enable = true;
+  hardware.pulseaudio.enable = false;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    media-session.config.bluez-monitor.rules = [
+      {
+        # Matches all cards
+        matches = [ { "device.name" = "~bluez_card.*"; } ];
+        actions = {
+          "update-props" = {
+            "bluez5.reconnect-profiles" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
+            # mSBC is not expected to work on all headset + adapter combinations.
+            "bluez5.msbc-support" = true;
+            # SBC-XQ is not expected to work on all headset + adapter combinations.
+            "bluez5.sbc-xq-support" = true;
+          };
+        };
+      }
+      {
+        matches = [
+          # Matches all sources
+          { "node.name" = "~bluez_input.*"; }
+          # Matches all outputs
+          { "node.name" = "~bluez_output.*"; }
+        ];
+        actions = {
+          "node.pause-on-idle" = false;
+        };
+      }
+    ];
+     
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.augustin = {
+     isNormalUser = true;
+     extraGroups = [ "wheel" "video" ]; # Enable ‘sudo’ for the user.
+  };
+
+  users.defaultUserShell = pkgs.zsh;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    atom
+    git
+    pass
+    gnome.gnome-tweaks
+    qrencode
+    yarn
+    htop
+    lm_sensors
+    unzip
+    irssi
+    wl-clipboard
+    nodejs
+    clinfo
+    wine-staging
+    winetricks
+    ethminer-free
+    liquidctl
+  ];
+
+  programs.steam.enable = true;
+
+  programs.zsh.enable = true;
+  programs.zsh.ohMyZsh.enable = true;
+  programs.zsh.ohMyZsh.theme = "steeef";
+  programs.zsh.ohMyZsh.plugins = [ "git" "kubectl" "pass" ];
+
+  environment.shellAliases = {
+    sensors = "watch -n 2 sensors";
+    lightning-effect-1 = "liquidctl set led2 color backwards-marquee-5 2f6017 --speed slowest";
+    lightning-effect-2 = "liquidctl set led2 color rainbow-flow --speed slowest";
+    qr = ''qrencode -m 2 -t utf8 <<< "$1"'';
+    bashtop = "bpytop";
+    switch = "sudo nixos-rebuild switch";
+  };
+
+  programs.gnupg.agent = {
+     enable = true;
+     enableSSHSupport = true;
+  };
+
+  # List services that you want to enable:
+  services.dnscrypt-proxy2 = {
+    enable = true;
+    settings = {
+      ipv6_servers = true;
+      require_dnssec = true;
+
+      sources.public-resolvers = {
+        urls = [
+          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+        ];
+        cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
+        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+      };
+
+      # You can choose a specific set of servers from https://github.com/DNSCrypt/dnscrypt-resolvers/blob/master/v3/public-resolvers.md
+      # server_names = [ ... ];
+    };
+  };
+
+  services.keybase.enable = true;
+  services.kbfs.enable = true; 
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  
+  networking.firewall.enable = true;
+
+  system.autoUpgrade.enable = true;
+  system.autoUpgrade.allowReboot = false;
+  system.stateVersion = "21.05";
+}
